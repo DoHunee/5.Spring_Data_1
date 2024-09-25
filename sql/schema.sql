1c7b1513794327e2fd86609c92dea8972ea1f9de
DoHunee <jehgns1@egmail.com>
2024-09-25 16:52:14 +0900

    트랜잭션 - 적용1

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,3 +1,4 @@
+-- Tavle 생성
 -- drop table member if exists cascade;
 create table member
 (
@@ -5,3 +6,32 @@ create table member
     money integer not null default 0,
     primary key (member_id)
 );
+
+
+
+set autocommit true; -- 자동 커밋 모드 설정
+insert into member(member_id, money) values ('data1',10000); -- 자동 커밋
+insert into member(member_id, money) values ('data2',10000); -- 자동 커밋
+
+
+set autocommit false; -- 수동 커밋 모드 설정
+insert into member(member_id, money) values ('data3',10000);
+insert into member(member_id, money) values ('data4',10000);
+commit; -- 수동 커밋
+
+
+
+-- 데이터 초기화
+set autocommit true;
+delete from member;
+insert into member(member_id, money) values ('oldId',10000);
+
+select * from member --데이터 확인
+
+
+-- 트랜잭션 시작
+set autocommit false; -- 수동 커밋 모드
+insert into member(member_id, money) values ('newId1',10000);
+insert into member(member_id, money) values ('newId2',10000);
+
+
