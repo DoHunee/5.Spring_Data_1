b32717dca7851ef30906bee566fff86f58389ebb
DoHunee <jehgns1@egmail.com>
2024-09-25 16:01:21 +0900

    DataSource 예제2 - 커넥션 풀

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -3,6 +3,9 @@ package hello.jdbc.connection;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import com.zaxxer.hikari.HikariDataSource;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,7 +14,6 @@ import static hello.jdbc.connection.ConnectionConst.*;
 
 @Slf4j
 public class ConnectionTest {
-
 
     // DriverManager를 사용하여 각각의 새로운 커넥션을 얻는 테스트 메서드
     @Test
@@ -30,7 +32,7 @@ public class ConnectionTest {
                 USERNAME, PASSWORD);
         useDataSource(dataSource);
     }
-    
+
     // DataSource를 사용하여 커넥션을 얻고 출력하는 메서드
     private void useDataSource(DataSource dataSource) throws SQLException {
         Connection con1 = dataSource.getConnection();
@@ -39,5 +41,18 @@ public class ConnectionTest {
         log.info("connection={}, class={}", con2, con2.getClass());
     }
 
+    // HikariDataSource(커넥션 풀)을 사용하여 커넥션을 얻고 출력하는 테스트 메서드
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링: HikariProxyConnection(Proxy) -> JdbcConnection(Target)
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+        useDataSource(dataSource);
+        Thread.sleep(1000); // 커넥션 풀에서 커넥션 생성 시간 대기
+    }
 
 }
