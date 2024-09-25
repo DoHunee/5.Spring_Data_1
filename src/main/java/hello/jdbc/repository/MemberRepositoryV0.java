9fff277c04a21d113ab9ad74c7327be9955bb239
DoHunee <jehgns1@egmail.com>
2024-09-25 15:00:36 +0900

    JDBC 개발 - 조회

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -4,6 +4,7 @@ import hello.jdbc.connection.DBConnectionUtil;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -11,6 +12,7 @@ import java.sql.*;
 @Slf4j
 public class MemberRepositoryV0 {
 
+    // JDBC 등록
     public Member save(Member member) throws SQLException {
         String sql = "insert into member(member_id, money) values(?, ?)";
         Connection con = null;
@@ -30,6 +32,35 @@ public class MemberRepositoryV0 {
         }
     }
 
+    // JDBC 조회
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" +
+                        memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
+        }
+    }
+
+    // JDBC 수정
     private void close(Connection con, Statement stmt, ResultSet rs) {
         if (rs != null) {
             try {
@@ -54,6 +85,7 @@ public class MemberRepositoryV0 {
         }
     }
 
+    // JDBC 삭제
     private Connection getConnection() {
         return DBConnectionUtil.getConnection();
     }
--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,16 +1,30 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
 import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
 class MemberRepositoryV0Test {
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
+        
         // save
         Member member = new Member("memberV0", 10000);
         repository.save(member);
+
+        // findById
+        //  테스트는 2번 실행하면 PK 중복 오류가 발생한다. 
+        // 이 경우 delete from member후에 재실행
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
+
 }
